5008d7bf4e5a3ced56a81e1212ef60f2ce5b57e2
Michael Candido <mcandido12@gmail.com>
2013-06-17 15:01:19 -0400

    now works correctly in chrome

--- libupload.js
+++ libupload.js
@@ -1,54 +1,56 @@
 
 var libupload = (function () {
 
-  function upload(form, onStart, onComplete) {
-
-    var document = form.ownerDocument;
-
+  function createIframe(document) {
     var iframe = document.createElement('iframe');
     iframe.style.height = 0;
     iframe.style.width = 0;
     iframe.style.border = 'none';
     iframe.name = 'libupload_iframe' + Date.now();
+    return iframe;
+  }
+
+  function formFileNames(form) {
+    var fileMap = {};
+    for (var i = 0; i < form.elements.length; i++) {
+      var input = form.elements[i];
+      if (input.type == 'file' && input.name)
+        fileMap[input.name] = input.value;
+    }
+    return fileMap;
+  }
+
+  function upload(form, onStart, onComplete) {
+
+    var document = form.ownerDocument, isSubmitted = false;
 
     function onload() {
+      if (!isSubmitted)
+        return;
+      
       form.reset();
-      if (iframe.removeEventListener)
-        iframe.removeEventListener('load', onload);
-      else if (iframe.detachEvent)
-        iframe.detachEvent(onload);
+      if (this.removeEventListener)
+        this.removeEventListener('load', onload);
+      else if (this.detachEvent)
+        this.detachEvent(onload);
 
-      if (iframe.contentDocument)
-        var response = iframe.contentDocument.body.innerHTML;
-      else if (iframe.contentWindow)
-        var response = iframe.contentWindow.document.body.innerHTML;
-      else if (iframe.document)
-        var response = iframe.document.body.innerHTML;
-
+      var response = this.contentDocument.body.innerHTML;
       if (onComplete)
         onComplete(response);
-
-      setTimeout(function () { iframe.parentNode.removeChild(iframe); }, 0);
+      
+      this.parentNode.removeChild(this);
     }
 
-    if (iframe.addEventListener)
-      iframe.addEventListener('load', onload);
-    else if (iframe.attachEvent)
-      iframe.attachEvent('onload', onload);
-
+    var iframe = createIframe(document);
+    iframe.onload = onload;
     form.target = iframe.name;
-    document.body.appendChild(iframe);
 
-    if (onStart) {
-      var fileMap = {};
-      for (var i = 0; i < form.elements.length; i++) {
-        var input = form.elements[i];
-        if (input.type == 'file' && input.name)
-          fileMap[input.name] = input.value;
-      }
-      onStart(fileMap);
-    }
+    if (onStart)
+      onStart(formFileNames(form));
+
+    document.body.appendChild(iframe);
     form.submit();
+    isSubmitted = true;
   }
 
   return {
